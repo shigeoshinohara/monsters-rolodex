f3ba516b0db736c2f7bb49d70185c697e0a0b370
ShigeoShinohara <s.shinohara@sync-web.jp>
2022-01-05 15:55:54 +0900

    add card component close #4

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import './card-list.styles.css';
+import {Card} from "../card/card.component";
 
-export const CardList = props => {
-    console.log(props);
-    return <div className="card-list">{props.children}</div>
-}
+export const CardList = props => (
+    <div className="card-list">
+        {
+            props.monsters.map(monster =>
+                <Card key={monster.id} monster={monster} />
+            )
+        }
+    </div>
+);
+
+
+
